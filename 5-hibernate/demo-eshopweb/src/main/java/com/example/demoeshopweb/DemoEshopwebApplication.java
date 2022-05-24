e23f28c8886fcaaeb95b71c232694742a25c59fa
Jirka Pinkas <jirka.pinkas@gmail.com>
2022-05-24 13:24:19 +0200

    hello world spring boot web

---
+++ 5-hibernate/demo-eshopweb/src/main/java/com/example/demoeshopweb/DemoEshopwebApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoeshopweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoEshopwebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoEshopwebApplication.class, args);
+    }
+
+}
