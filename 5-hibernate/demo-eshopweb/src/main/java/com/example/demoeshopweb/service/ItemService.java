4c0f087f45c507347573e1a5026d7212ecc549f4
Jirka Pinkas <jirka.pinkas@gmail.com>
2022-05-24 15:19:59 +0200

    + slf4j (lombok)

--- 5-hibernate/demo-eshopweb/src/main/java/com/example/demoeshopweb/service/ItemService.java
+++ 5-hibernate/demo-eshopweb/src/main/java/com/example/demoeshopweb/service/ItemService.java
@@ -5,12 +5,14 @@ import com.example.demoeshopweb.entity.Item;
 import com.example.demoeshopweb.mapper.ItemMapper;
 import com.example.demoeshopweb.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class ItemService {
 
